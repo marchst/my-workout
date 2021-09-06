d5bc3e7dd9f976751f7c90d7d1ede9b1f5471ab2
Surapunya Thongdee <marchsrp@gmail.com>
2021-09-06 13:40:22 +0700

    Initial Commit

---
+++ MyWorkout/MyWorkoutApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyWorkoutApp.swift
+//  MyWorkout
+//
+//  Created by Surapunya Thongdee on 6/9/2564 BE.
+//
+
+import SwiftUI
+
+@main
+struct MyWorkoutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
